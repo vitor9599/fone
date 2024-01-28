94609181972e7448076c3bfc9f0c6c63efc4a385
Vitor Lopes Garcia <138633840+vitor9599@users.noreply.github.com>
2024-01-28 14:59:33 -0300

    Add files via upload
    Site fone ouvido ,com HTML, CSS, JAVA SCRIPTY.

---
+++ scripty.js
@@ -0,0 +1,29 @@
+
+let fone = document.querySelector(".one")
+
+let prata =document.querySelector(".row")
+let azul = document.querySelector(".row-2")
+let vermelho = document.querySelector(".row-3")
+
+prata.addEventListener("click",()=>{
+    fone.src ="./img/prata.png"
+})
+azul.addEventListener("click",()=>{
+    fone.src ="./img/azul.png"
+})
+vermelho.addEventListener("click",()=>{
+    fone.src = "./img/vermelho.png"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
